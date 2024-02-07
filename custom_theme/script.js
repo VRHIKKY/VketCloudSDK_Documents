80af05bc8381a7c71642d323ec5c0c12671ef993
rakai <akyu00627@gmail.com>
2024-02-07 15:49:59 +0900

    スクリプトを更新

--- custom_theme/script.js
+++ custom_theme/script.js
@@ -16,27 +16,31 @@ const createChatLi = (message, className) => {
     return chatLi; // return chat <li> element
 }
 const generateResponse = (chatElement) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const API_URL = "http://127.0.0.1:49515/prompt";
     const messageElement = chatElement.querySelector("p");
-    // Define the properties and message for the API request
+    const params = new URLSearchParams();
+    params.append('prompt', userMessage);
+
     const requestOptions = {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${API_KEY}`
+            "Content-Type": "application/x-www-form-urlencoded"
         },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: userMessage}],
-        })
+        body: params
     }
     // Send POST request to API, get response and set the reponse as paragraph text
-    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-        messageElement.textContent = data.choices[0].message.content.trim();
-    }).catch(() => {
+    fetch(API_URL, requestOptions)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        messageElement.textContent = data.response;
+    })
+    .catch(error => {
         messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
-    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+        messageElement.textContent = "Oops! Something went wrong. Please try again."
+    })
+    .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
 }
 const handleChat = () => {
     userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
@@ -50,7 +54,7 @@ const handleChat = () => {
     
     setTimeout(() => {
         // Display "Thinking..." message while waiting for the response
-        const incomingChatLi = createChatLi("Thinking...", "incoming");
+        const incomingChatLi = createChatLi("考え中...", "incoming");
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
         generateResponse(incomingChatLi);
